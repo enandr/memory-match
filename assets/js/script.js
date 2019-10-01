b8fae1a544661e099a8d005dddc525ed81c8dd81
Roger Enand <rogerenand@gmail.com>
2019-09-30 23:13:09 -0700

    created html skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(initializeApp);
 
+function initializeApp(){
+  var selected = $('.cardBack');
+  var images = ["assets/images/react-logo.png", "assets/images/css-logo.png", "assets/images/html-logo.png", "assets/images/js-logo.png", "assets/images/mysql-logo.jpg", "assets/images/node-logo.png", "assets/images/php-logo.jpeg", "assets/images/docker-logo.jpg","assets/images/gitHub-logo.png"]
+  for (var selector = 0,img = 0;selector<=16;selector+=2,img++){
+    $(selected[selector]).css({ 'background-image': 'url(' + images[img] + ')' });
+  }
+
+
+}
